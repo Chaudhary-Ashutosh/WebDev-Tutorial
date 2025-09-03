517da0630a35ddca483de001dfc630c52ad3fb3d
Chaudhary-Ashutosh <Ashutoshchaudhary2710@gmail.com>
2025-09-03 11:29:08 +0530

    Node.js Basics

---
+++ Back-end/node.js/script.js
@@ -0,0 +1,22 @@
+// let m = "message";
+// for(let i=1; i<6; i++){
+//     console.log(m, i);
+// }
+// console.log("Bye!")
+
+// //console.log(process.argv);
+
+// let args = process.argv;
+// for(let i=2; i<args.length; i++){
+//     console.log("hello to ",args[i]);
+// }
+
+// const math = require("./math");
+// console.log(math);
+// console.log(math.sum(2,2));
+// console.log(math.mul(2,5));
+// console.log(math.PI);
+// console.log(math.g);
+
+const info = require("./Fruits");
+console.log(info);
