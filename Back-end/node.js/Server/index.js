ef8f571484ad2a096049b255f2b53f89a925d46a
Chaudhary-Ashutosh <Ashutoshchaudhary2710@gmail.com>
2025-09-15 10:07:32 +0530

    https server

--- Back-end/node.js/Server/index.js
+++ Back-end/node.js/Server/index.js
@@ -1,7 +1,25 @@
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
 const myServer = http.createServer((req,res)=>{
-    console.log("New Request Rec.");
-    res.end("Hello From Server");
+    if(req.url === "/favicon.ico") return res.end();
+    const log = `${Date.now()}: ${req.url} New Req Received\n`
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
+    fs.appendFile("log.txt",log,(err,data)=>{
+        switch(myUrl.pathname){
+            case '/' :res.end("HomePage");
+            break;
+            case '/about':
+                const username = myUrl.query.myname;
+                res.end(`Hi ${username}`)
+            break;
+            case '/search':
+                const search = myUrl.query.search_query;
+                res.end("Here is your result for " + search );
+            default: res.end("404 Not Found");
+        }
+        
+    });
 });
-
 myServer.listen(8000,() => console.log("Server Started"));
